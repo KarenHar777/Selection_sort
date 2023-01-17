ba8c85e8a93af2381a760623e9a7da072b8cd816
KarenHarutyunyan <karenvardanovich@gmail.com>
2023-01-18 00:06:06 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] num = new int[] { 7, 2, 3, 5, 9, 1, 4 };
+
+        foreach (int j in num)
+        {
+            Console.Write(j + "\t");
+        }
+
+        int sort=0;
+        int cloud;
+        while (sort < num.Length)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[sort] > num[i])
+                {
+                    cloud = num[sort];
+                    num[sort] = num[i];
+                    num[i] = cloud;
+                }
+
+            }
+            sort++;
+        }
+        Console.WriteLine();
+
+        foreach (int j in num)
+            {
+                Console.Write(j+"\t");
+            }
+    }
+}
